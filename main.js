35b97890d17dde3e23e9b88b6510d4f5a83903e6
randyttphan <randyttphan@gmail.com>
2020-02-01 13:26:48 -0600

    getting all dom elements to work with and main function to update

---
+++ main.js
@@ -0,0 +1,9 @@
+const currencyElement1 = document.getElementById("currency-one");
+const amountElement1 = document.getElementById("amount-one");
+const currencyElement2 = document.getElementById("currency-two");
+const amountElement2 = document.getElementById("amount-two");
+const rate = document.getElementById("rate");
+const convertBtn = document.getElementById("convert");
+
+//Fetch exchange rates and update the DOM
+function calculate() {}
